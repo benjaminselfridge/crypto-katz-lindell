50cbaaae1074766eec433eae8d1867f95fff7434
Ben Selfridge <benselfridge@galois.com>
2021-04-15 08:54:59 -0700

    Modifies Alpha interface slightly

--- src/Crypto/Types.hs
+++ src/Crypto/Types.hs
@@ -7,8 +7,12 @@ module Crypto.Types
   ( -- * Text types
     Alpha(..)
   , alphaFromChar
+  , alphaFromString
   , alphaToChar
   ) where
+
+import Data.Char (toUpper)
+import Data.Maybe (catMaybes)
 
 -- | Alphabetical character, for schemes that only use letters.
 data Alpha = A | B | C | D | E | F | G | H
@@ -19,34 +23,34 @@ data Alpha = A | B | C | D | E | F | G | H
 
 -- | Convert a 'Char' to an 'Alpha'. Throw error if the input is not one of the
 -- 26 uppercase letters.
-alphaFromChar :: Char -> Alpha
-alphaFromChar 'A' = A
-alphaFromChar 'B' = B
-alphaFromChar 'C' = C
-alphaFromChar 'D' = D
-alphaFromChar 'E' = E
-alphaFromChar 'F' = F
-alphaFromChar 'G' = G
-alphaFromChar 'H' = H
-alphaFromChar 'I' = I
-alphaFromChar 'J' = J
-alphaFromChar 'K' = K
-alphaFromChar 'L' = L
-alphaFromChar 'M' = M
-alphaFromChar 'N' = N
-alphaFromChar 'O' = O
-alphaFromChar 'P' = P
-alphaFromChar 'Q' = Q
-alphaFromChar 'R' = R
-alphaFromChar 'S' = S
-alphaFromChar 'T' = T
-alphaFromChar 'U' = U
-alphaFromChar 'V' = V
-alphaFromChar 'W' = W
-alphaFromChar 'X' = X
-alphaFromChar 'Y' = Y
-alphaFromChar 'Z' = Z
-alphaFromChar c = error $ "alphaFromChar \'" ++ [c] ++ "' :: Alpha not implemented"
+alphaFromChar :: Char -> Maybe Alpha
+alphaFromChar 'A' = Just A
+alphaFromChar 'B' = Just B
+alphaFromChar 'C' = Just C
+alphaFromChar 'D' = Just D
+alphaFromChar 'E' = Just E
+alphaFromChar 'F' = Just F
+alphaFromChar 'G' = Just G
+alphaFromChar 'H' = Just H
+alphaFromChar 'I' = Just I
+alphaFromChar 'J' = Just J
+alphaFromChar 'K' = Just K
+alphaFromChar 'L' = Just L
+alphaFromChar 'M' = Just M
+alphaFromChar 'N' = Just N
+alphaFromChar 'O' = Just O
+alphaFromChar 'P' = Just P
+alphaFromChar 'Q' = Just Q
+alphaFromChar 'R' = Just R
+alphaFromChar 'S' = Just S
+alphaFromChar 'T' = Just T
+alphaFromChar 'U' = Just U
+alphaFromChar 'V' = Just V
+alphaFromChar 'W' = Just W
+alphaFromChar 'X' = Just X
+alphaFromChar 'Y' = Just Y
+alphaFromChar 'Z' = Just Z
+alphaFromChar c = Nothing
 
 -- | Convert an 'Alpha' to a 'Char'.
 alphaToChar :: Alpha -> Char
@@ -76,3 +80,8 @@ alphaToChar W = 'W'
 alphaToChar X = 'X'
 alphaToChar Y = 'Y'
 alphaToChar Z = 'Z'
+
+-- | Converts a string to a list of 'Alpha's by mapping the characters to
+-- uppercase and removing characters that are not alphabetical.
+alphaFromString :: String -> [Alpha]
+alphaFromString = catMaybes . map (alphaFromChar . toUpper)
