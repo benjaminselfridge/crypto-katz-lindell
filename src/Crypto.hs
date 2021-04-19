de2bd31a9c3f914bd89c182928ef0a03b8612ae3
Ben Selfridge <benselfridge@galois.com>
2021-04-19 14:48:27 -0700

    Adds Utils to top-level module

--- src/Crypto.hs
+++ src/Crypto.hs
@@ -4,8 +4,10 @@ module Crypto
   ( module Crypto.Types
   , module Crypto.Schemes
   , module Crypto.Attacks
+  , module Crypto.Utils
   ) where
 
 import Crypto.Types
 import Crypto.Schemes
 import Crypto.Attacks
+import Crypto.Utils
